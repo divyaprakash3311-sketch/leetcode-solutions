128fc05f71565a3c712e69019c3f12853f123abe
DIVYADHARSHINI G <divyaprakash3311@gmail.com>
2026-09-10 23:02:15 +0530

    Time: 44 ms (34.84%) | Memory: 47.1 MB (40.91%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -7,7 +7,7 @@ class Solution {
 
             for (int j = i + 1; j < nums.length; j++) {
                 
-                if (a + nums[j] == target) {
+                if (nums[i]+ nums[j] == target) {
                     return new int[]{i, j};
                 }
             }
